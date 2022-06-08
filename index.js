4dad34a7d25b74acb992ef2ba5a6b9791723b375
Gab1nnn <107124229+Gab1nnn@users.noreply.github.com>
2022-06-08 14:16:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+function blob(elem)
+{
+	var img = document.getElementById("blob");
+	var nimg = img.getAttribute("src");
+	nimg= "Frontblob.jpg";
+	img.setAttribute("src", nimg);
+}
+
+function blob2(elem)
+{
+	var img = document.getElementById("blob");
+	var nimg = img.getAttribute("src");
+	nimg= "Back_blob.jpg";
+	img.setAttribute("src", nimg);
+}
+
+
+function ludis(elem)
+{
+	var img = document.getElementById("ludis");
+	var nimg = img.getAttribute("src");
+	nimg= "Ludis.jpg";
+	img.setAttribute("src", nimg);
+}
+
+function ludis2(elem)
+{
+	var img = document.getElementById("ludis");
+	var nimg = img.getAttribute("src");
+	nimg= "back_ludis.jpg";
+	img.setAttribute("src", nimg);
+}
